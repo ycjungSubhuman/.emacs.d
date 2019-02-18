cf329dfaf12552a2a16df5f3ae0797c5b5836a5d
ycjungSubhuman <ycjung@postech.ac.kr>
2019-02-18 10:18:04 +0000

    Modify init-tools

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/lisp")
 (require 'init-tools)
 
 ;; load script modules from subfolders
-(init-mod '("package" "editor", "custom"))
+(init-mod '("package" "editor" "custom"))
--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -5,5 +5,7 @@
                        (load-file (concat (file-name-as-directory dir) f)))))
     (mapc load-single (directory-files dir nil "\\.el$"))))
 
-(defun init-mod (dirs)
-  (mapc init-load-dir dirs))
+(defun init-mod (names)
+  (let ((dirs 
+          (mapcar (lambda (n) (concat (file-name-as-directory "~/.emacs.d") n)) names)))
+      (mapc 'init-load-dir dirs)))
