13c208e888655ffae2558bfaa2f2164c33e7ad7c
ycjungSubhuman <ycjung@postech.ac.kr>
2019-04-01 13:15:59 +0900

    Unbind C-x C-c exit

--- keymaps/user.el
+++ keymaps/user.el
@@ -1,5 +1,9 @@
+;; my preference
+(global-unset-key (kbd "C-x C-c")) ;; disable exit
+
 (global-set-key (kbd "C-x [") 'previous-buffer)
 (global-set-key (kbd "C-x ]") 'next-buffer)
 
 (global-set-key (kbd "C-S-f e") 'dotfile-reload)
 (global-set-key (kbd "C-S-f r") 'rename-buffer-file)
+
